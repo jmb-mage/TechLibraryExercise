25b9bc8c26d167cf17c7175f4f492ef52dad8aef
Jonathan Matthew Beck <50887898+jmb-mage@users.noreply.github.com>
2021-04-21 11:13:58 -0400

    cleanup ui

--- TechLibrary.Web/src/App.vue
+++ TechLibrary.Web/src/App.vue
@@ -4,7 +4,7 @@
             <b-navbar variant="lg">
                 <b-navbar-nav>
                     <b-nav-item href="/">Home</b-nav-item>
-                    <b-nav-item v-if="this.$route.name != 'book_edit'" :to="{ name: 'book_add' }">Add</b-nav-item>
+                    <b-nav-item :disabled="this.$route.name == 'book_edit'" :to="{ name: 'book_add' }">Add Book</b-nav-item>
                 </b-navbar-nav>
             </b-navbar>
             <router-view> </router-view>
